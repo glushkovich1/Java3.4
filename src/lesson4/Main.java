7c485a700abb685040c8702576e214d5f346a254
glushkovich1 <glushkovich1@gmail.com>
2020-09-30 21:34:22 +0300

    HomeWork4

--- src/lesson4/Main.java
+++ src/lesson4/Main.java
@@ -2,7 +2,43 @@ package lesson4;
 
 public class Main {
 
+    static Object lock = new Object();
+    static volatile char c = 'A';
+
+    public static class Run implements Runnable {
+        private char currentSymbol;
+        private char nextSymbol;
+        private int count;
+
+        public Run(char currentSymbol, char nextSymbol, int count) {
+            this.currentSymbol = currentSymbol;
+            this.nextSymbol = nextSymbol;
+            this.count = count;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < count; i++) {
+                synchronized (lock) {
+                    try {
+                        while (c != currentSymbol)
+                            lock.wait();
+                        System.out.print(currentSymbol);
+                        c = nextSymbol;
+                        lock.notifyAll();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
-	// write your code here
+        Object lock = new String();
+        new Thread(new Run('A', 'B', 5)).start();
+        new Thread(new Run('B', 'C', 5)).start();
+        new Thread(new Run('C', 'A', 5)).start();
+
     }
 }
